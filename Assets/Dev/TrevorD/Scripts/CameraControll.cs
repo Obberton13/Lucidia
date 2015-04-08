db5d1fff6ad3cd1c02058f2cfc71985b79ab7998
TheDerpySage <majoradecamp@gmail.com>
2015-04-08 18:13:15 -0500

    Added my contribution.
    Here I am. Just did some quick scripting for player movement on my
    little Capboy. Granted the script uses velocity pushes rather than
    literal transforms over time. I will change that later. It also uses
    literal keybinds.
    The PlayerController I wrote has the ability to sprint while the left
    shift is held down, and double jump.
    Feel free to check it out and make changes as you see fit.

---
+++ Assets/Dev/TrevorD/Scripts/CameraControll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControll : MonoBehaviour {
+
+	private PlayerController player;
+	private Vector3 viewPos;
+	
+	
+	public void Start () {
+		player = FindObjectOfType<PlayerController> ();
+	}
+	
+	
+	public void Update () {
+		viewPos = new Vector3(player.transform.position.x, transform.position.y, (player.transform.position.z - 16));
+		transform.position = viewPos;
+	}
+}
